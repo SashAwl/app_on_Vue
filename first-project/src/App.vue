0f734ee77258e76876b24e2becdd1f9199b6da5b
SashAwl <aleks.shilova@gmail.com>
2023-07-14 22:38:03 +0300

    created project Vue CLI

---
+++ first-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue welcome" src="./assets/img1.jpg">
+    <HelloComponent msg="Приветствем вас великодушно!" />
+    <SliderCat/>
+  </div>
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+import SliderCat from './components/SliderCat.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloComponent,
+    SliderCat
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ first-project/src/components/SliderCat.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="center">
+    <img src="../assets/cat1.jpg" alt="cat" class="big-img">
+    <div class="collection">
+      <img @click="makeBig" v-for="img in listImg" :key="img.id" :src="getImgUrl(img)" alt="cat" class="cat-item">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SliderCat',
+  props: {},
+  data () {
+    return {
+      listImg: ['cat1.jpg', 'cat2.jpg', 'cat3.jpg'],
+      currentImg: 6
+    }
+  },
+  methods: {
+    makeBig () {
+
+    },
+    getImgUrl (img) {
+      console.log(`../assets/${img}`)
+      return `../assets/${img}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.center{
+  max-width: 1300px;
+}
+.collection{
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  padding-bottom: 30px;
+  padding-top: 30px;
+}
+.cat-item {
+    width: 200px;
+    border: 1px solid rgb(148, 144, 144);
+    border-radius: 20px ;
+}
+.big-img {
+  border: 10px solid grey;
+  border-radius: 20px;
+}
+</style>
